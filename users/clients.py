1b858dbd83213c6489d0018488f05d87c5f1f08d
panford <panfordkobby@gmail.com>
2024-06-11 10:35:06 +0200

    Commit new structure

--- users/clients.py
+++
@@ -1,7 +0,0 @@
-from users.annotator import AnnotatorRepository
-from users.reviewer import ReviewerRepository
-
-class Client(AnnotatorRepository, ReviewerRepository):
-  def __init__(self, session) -> None:
-    super().__init__(session)
-    super(AnnotatorRepository, self).__init__(session)
