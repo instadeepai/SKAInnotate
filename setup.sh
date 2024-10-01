0161aef2e6a154546eb328a269ae65e6fa3b831d
panford <panfordkobby@gmail.com>
2024-10-01 11:10:50 +0200

    Update setup script

--- setup.sh
+++ setup.sh
@@ -6,11 +6,7 @@ command -v node >/dev/null 2>&1 || { echo "Node.js is required. Please install N
 command -v npm >/dev/null 2>&1 || { echo "npm is required. Please install npm."; exit 1; }
 command -v uvicorn >/dev/null 2>&1 || { echo "Installing Uvicorn..."; pip install uvicorn; }
 
-echo "Cloning the repository..."
-git clone https://github.com/instadeepai/SKAInnotate.git
-
-echo "Navigating to the frontend directory..."
-cd SKAInnotate/setup/frontend || { echo "Failed to navigate to frontend directory"; exit 1; }
+cd setup/frontend || { echo "Failed to navigate to frontend directory"; exit 1; }
 
 echo "Installing and building npm packages and files..."
 npm install
