7bac6f7f6b91a6aac95f090c1fab3577693b2b40
panford <panfordkobby@gmail.com>
2024-10-02 13:06:10 +0200

    Update readme and setup app

--- setup/frontend/src/App.js
+++ setup/frontend/src/App.js
@@ -1,7 +1,7 @@
-// src/App.js
 import React, { useState } from 'react';
 import './App.css';
 import SetupForm from './components/SetupForm'; // Import the SetupForm component
+import 'semantic-ui-css/semantic.min.css';
 
 function App() {
   return (
